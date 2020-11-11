5a7d2f8f140b9b7e152d598773fb3a69f68f61dc
Kolja Leonard Blauhut <blauhut@campus.tu-berlin.de>
2020-11-11 17:43:59 +0100

    -Really bad fix but it works

--- chrome/bgrequests.js
+++ chrome/bgrequests.js
@@ -31,16 +31,16 @@ async function returnData(id, port) {
     if (port.name == "eu") {
       userData = await etf2lUserData(id);
       if (userData.registered == false || userData.data.division == null)
-      userData = await rglUserData(id);
-      if (userData.registered == false || userData.data.division == null)
-        userData = await eseaUserData(id);
+        userData = await rglUserData(id);
+      //if (userData.registered == false || userData.data.division == null)
+      //userData = await eseaUserData(id);
       // if (userData.registered == false || userData.data.division == null)
       //   userData = await ozfUserData(id);
     } else if (port.name == "na") {
       userData = await rglUserData(id);
       if (userData.registered == false || userData.data.division == null)
-      userData = await eseaUserData(id);
-      if (userData.registered == false || userData.data.division == null)
+        //userData = await eseaUserData(id);
+        //if (userData.registered == false || userData.data.division == null)
         userData = await etf2lUserData(id);
       // if (userData.registered == false || userData.data.division == null)
       //   userData = await ozfUserData(id);
@@ -48,8 +48,8 @@ async function returnData(id, port) {
       // userData = await ozfUserData(id);
       //if (userData.registered == false || userData.data.division == null)
       userData = await rglUserData(id);
-      if (userData.registered == false || userData.data.division == null)
-      userData = await eseaUserData(id);
+      //if (userData.registered == false || userData.data.division == null)
+      //userData = await eseaUserData(id);
       if (userData.registered == false || userData.data.division == null)
         userData = await etf2lUserData(id);
     }
@@ -78,6 +78,7 @@ async function request(url) {
     headers
   });
   return await response.json();
+
 }
 
 function etf2lUserData(id) {
